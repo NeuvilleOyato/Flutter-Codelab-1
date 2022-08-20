170910d25b34bf36633566c209bee9e6ccb20da3
NeuvilleOyato <65784244+NeuvilleOyato@users.noreply.github.com>
2022-08-20 18:53:02 +0300

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:startup_namer/random_words.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Startup Name Generator',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Startup Name Generator'),
+        ),
+        body: const Center(child: RandomWords()),
+      ),
+    );
+  }
+}
